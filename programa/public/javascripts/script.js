05d42d04a16968b067dadaeac73e4b7b695eeb21
TadasLekerauskas <159998656+TadasLekerauskas@users.noreply.github.com>
2024-11-21 18:21:39 +0200

    Veikia mqtt subscribas
    Veikia

--- programa/public/javascripts/script.js
+++
@@ -1,2 +0,0 @@
-//console.log("Scriptas veikia!");
-const mqtt = require('mqtt');
--- programa/routes/index.js
+++ programa/routes/index.js
@@ -2,32 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 const mqtt = require('mqtt')
-
-
-var topic = 'mqtt/kiberfizines'
-var topicFoto = 'info/fotorezistorius'
-var topicDHT = 'info/dhtSensorius'
-
-const client = mqtt.connect("mqtt://broker.hivemq.com")
-
-client.on('connect', () => {
-  console.log('Connected')
-  client.subscribe([topic, topicFoto, topicDHT], () => {
-    console.log(`Subscribe to topic '${topic}', topic '${topicFoto}' and topic '${topicDHT}`)
-  })
-})
-
-client.on('message', (topic, payload) => {
-  console.log('Received Message:', topic, payload.toString())
-})
-
-client.on('disconnect', () => {
-  console.log("Atsijunge");
-})
+let once = true;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var db = req.db;
+  if(once){
+    subscribe(db);
+    once = false;
+  }
 
   let photoresistor = -1;
   let temperature = -100;
@@ -45,9 +28,53 @@ router.get('/', function(req, res, next) {
       console.log("Nera fotorezistoriaus");
     }
   })
-});
+})
+
+router.post('/', function(req, res, next) {
+  var db = req.db;
+})
 
 module.exports = router;
+
+function subscribe(db){
+  var topicFoto = 'info/fotorezistorius'
+  var topicDHT = 'info/dhtSensorius'
+
+  const client = mqtt.connect("mqtt://broker.hivemq.com")
+
+  client.on('connect', () => {
+    console.log('Connected')
+    client.subscribe([topicFoto, topicDHT], () => {
+      console.log(`Subscribe to topic '${topicFoto}' and topic '${topicDHT}`)
+    })
+  })
+
+  client.on('message', (topic, payload) => {
+    console.log('Received Message:', topic, payload.toString())
+    if (topic == 'info/fotorezistorius'){
+      putPhotorezistor(db, payload, function(rez){
+        if(rez != null)
+        {
+          console.log("Neatnaujinti fotorezistoriaus duomenys");
+        }
+      })
+    }
+    else if (topic == 'info/dhtSensorius'){
+      let sk = payload.toString();
+      let arr = sk.split(' ');
+      putTempHum(db, arr[0], arr[1],function(rez){
+        if(rez != null)
+        {
+          console.log("Neatnaujinti temperaturos ir dregmes duomenys");
+        }
+      })
+    }
+  })
+
+  client.on('disconnect', () => {
+    console.log("Atsijunge");
+  })
+}
 
 function getAllData(db, callback)
 {
@@ -63,4 +90,28 @@ function getAllData(db, callback)
           return callback(rows);
       }
   });
+}
+
+function putPhotorezistor(db, photoData, callback){
+  db.run("update Fotorezistorius set Reiksme = ?; ", photoData, function(updateError)
+    {
+        //jei nebuvo jokios klaidos updateError=null
+        //jei buvo klaida updateError=klaidos_pranesimas
+        return callback(updateError);  
+    });
+}
+
+function putTempHum(db, tempData, humData, callback){
+  db.run("update Temperatura set Reiksme = ?; ", tempData, function(updateError)
+    {
+        //jei nebuvo jokios klaidos updateError=null
+        //jei buvo klaida updateError=klaidos_pranesimas
+        return callback(updateError);  
+    });
+    db.run("update Dregme set Reiksme = ?; ", humData, function(updateError)
+    {
+        //jei nebuvo jokios klaidos updateError=null
+        //jei buvo klaida updateError=klaidos_pranesimas
+        return callback(updateError);  
+    });
 }
