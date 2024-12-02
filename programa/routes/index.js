26d17fb6743f684669d89cb4692f31e78b3be58d
TadasLekerauskas <159998656+TadasLekerauskas@users.noreply.github.com>
2024-12-02 17:32:12 +0200

    Bug fix and addition of new page
    Fixed bug about client and added registration page beggining

--- programa/routes/index.js
+++ programa/routes/index.js
@@ -3,35 +3,21 @@ var router = express.Router();
 
 const mqtt = require('mqtt')
 let once = true;
+let clientConnected = false;
 let client;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var db = req.db;
-  if(once){
-    subscribe(db);
-    once = false;
-  }
-
-  let photoresistor = -1;
-  let temperature = -100;
-  let humidity = -1;
-  // padaryta taip nes vienas
-  getAllData(db, function(data){
-    if(data != null){
-      //console.log(data[0]);
-      photoresistor = data[0].ftRk;
-      temperature = data[0].tmpRk;
-      humidity = data[0].drgRk;
-      res.render('index', {fotorezistorius: photoresistor, temperatura: temperature, dregme: humidity});
-    }
-    else{
-      console.log("Nera duomenu");
-    }
-  })
+  res.render('signIn');
 })
 
 router.post('/', function(req, res, next) {
+  console.log("Post metodas vyksta!");
+  res.render('index', {fotorezistorius: photoresistor, temperatura: temperature, dregme: humidity});
+})
+
+router.post('/siltnamis', function(req, res, next) {
   //console.log(req.body);
   var db = req.db;
   if (req.body.submit == 'Patvirtinti'){
@@ -55,28 +41,72 @@ router.post('/', function(req, res, next) {
   }
 })
 
+router.get('/siltnamis', function(req, res) {
+  var db = req.db;
+  if(once){
+    subscribe(db);
+    connectPostClient();
+    once = false;
+  }
+
+  let photoresistor = -1;
+  let temperature = -100;
+  let humidity = -1;
+  // padaryta taip nes vienas
+  getAllData(db, function(data){
+    if(data != null){
+      //console.log(data[0]);
+      photoresistor = data[0].ftRk;
+      temperature = data[0].tmpRk;
+      humidity = data[0].drgRk;
+      res.render('index', {fotorezistorius: photoresistor, temperatura: temperature, dregme: humidity});
+    }
+    else{
+      console.log("Nera duomenu");
+    }
+  })
+})
+
 module.exports = router;
+
+function connectClient(){
+  client = mqtt.connect("mqtt://broker.hivemq.com");
+
+  client.on('connect', () => {
+    clientConnected = true;
+    console.log("Client connected");
+  })
+  client.on('disconnect', () => {
+    console.log("Atsijunge");
+  })
+}
 
 function sendMessageRibos(fotoRiba, tmpVirsRiba, tmpApacRiba){
   let topic1 = 'command/foto';
   let topic2 = 'command/tmpVir';
   let topic3 = 'command/tmpApac';
+  if (clientConnected){
+    console.log("Zinutes issiustos!");
+    client.publish(topic1, fotoRiba, { qos: 0, retain: false }, (error) => {
+      if (error) {
+        console.error(error)
+      }
+    })
+    client.publish(topic2, tmpVirsRiba, { qos: 0, retain: false }, (error) => {
+      if (error) {
+        console.error(error)
+      }
+    })
+    client.publish(topic3, tmpApacRiba, { qos: 0, retain: false }, (error) => {
+      if (error) {
+        console.error(error)
+      }
+    })
+  } else{
+    console.log("Unable to send message, client not connected! \nRetrying connection");
+    connectClient();
+  }
 
-  client.publish(topic1, fotoRiba, { qos: 0, retain: false }, (error) => {
-    if (error) {
-      console.error(error)
-    }
-  })
-  client.publish(topic2, tmpVirsRiba, { qos: 0, retain: false }, (error) => {
-    if (error) {
-      console.error(error)
-    }
-  })
-  client.publish(topic3, tmpApacRiba, { qos: 0, retain: false }, (error) => {
-    if (error) {
-      console.error(error)
-    }
-  })
 
 }
 
@@ -84,40 +114,36 @@ function subscribe(db){
   var topicFoto = 'info/fotorezistorius'
   var topicDHT = 'info/dhtSensorius'
 
-  client = mqtt.connect("mqtt://broker.hivemq.com")
-
-  client.on('connect', () => {
-    console.log('Connected')
-    client.subscribe([topicFoto, topicDHT], () => {
+  if (postClientConnected){
+    postClient.subscribe([topicFoto, topicDHT], () => {
       console.log(`Subscribe to topic '${topicFoto}' and topic '${topicDHT}`)
     })
-  })
 
-  client.on('message', (topic, payload) => {
-    console.log('Received Message:', topic, payload.toString())
-    if (topic == 'info/fotorezistorius'){
-      putPhotorezistor(db, payload, function(rez){
-        if(rez != null)
-        {
-          console.log("Neatnaujinti fotorezistoriaus duomenys");
-        }
-      })
-    }
-    else if (topic == 'info/dhtSensorius'){
-      let sk = payload.toString();
-      let arr = sk.split(' ');
-      putTempHum(db, arr[0], arr[1],function(rez){
-        if(rez != null)
-        {
-          console.log("Neatnaujinti temperaturos ir dregmes duomenys");
-        }
-      })
-    }
-  })
-
-  client.on('disconnect', () => {
-    console.log("Atsijunge");
-  })
+    postClient.on('message', (topic, payload) => {
+      console.log('Received Message:', topic, payload.toString())
+      if (topic == 'info/fotorezistorius'){
+        putPhotorezistor(db, payload, function(rez){
+          if(rez != null)
+          {
+            console.log("Neatnaujinti fotorezistoriaus duomenys");
+          }
+        })
+      }
+      else if (topic == 'info/dhtSensorius'){
+        let sk = payload.toString();
+        let arr = sk.split(' ');
+        putTempHum(db, arr[0], arr[1],function(rez){
+          if(rez != null)
+          {
+            console.log("Neatnaujinti temperaturos ir dregmes duomenys");
+          }
+        })
+      }
+    })
+  } else{
+    console.log("Unable to subscribe to topics, client not connected! \nRetrying connection");
+    connectClient();
+  }
 }
 
 function getAllData(db, callback)
